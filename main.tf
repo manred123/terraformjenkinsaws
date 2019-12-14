a670ce566427715eb379417a00deca09bb7d0ef5
manred123 <manohar.karakondu@gmail.com>
2019-12-14 18:58:40 +0530

    update

--- main.tf
+++ main.tf
@@ -17,8 +17,8 @@ resource "aws_instance" "default" {
   ami                    = "${var.ami}"
   count                  = "${var.count}"
 #  key_name               = "${var.key_name}"
-  vpc_security_group_ids = ["${aws_security_group.default.id}"]
-  source_dest_check      = false
+#  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+#  source_dest_check      = false
   instance_type          = "${var.instance_type}"
 
   tags {
