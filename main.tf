52bf8bd62aab0e6f1264449636109ad87e4acb66
manred123 <manohar.karakondu@gmail.com>
2019-12-14 19:08:08 +0530

    update required changes

--- main.tf
+++ main.tf
@@ -16,9 +16,9 @@ provider "aws" {
 resource "aws_instance" "default" {
   ami                    = "${var.ami}"
   count                  = "${var.count}"
-#  key_name               = "${var.key_name}"
-#  vpc_security_group_ids = ["${aws_security_group.default.id}"]
-#  source_dest_check      = false
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  source_dest_check      = false
   instance_type          = "${var.instance_type}"
 
   tags {
